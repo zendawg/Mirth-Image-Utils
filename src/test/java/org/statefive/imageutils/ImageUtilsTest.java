ddbb30a8dd358e5127bd01383659d7bc0fcd1f31
zendawg <rich@statefive.org>
2013-03-25 15:23:43 +0000

    Initial sources for image encoding.

---
+++ src/test/java/org/statefive/imageutils/ImageUtilsTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.statefive.imageutils;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rich
+ */
+public class ImageUtilsTest {
+    
+    public ImageUtilsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of scale method, of class ImageUtils.
+     */
+    @Test
+    public void testScale() throws Exception {
+        System.out.println("scale");
+        String src = "";
+        int destWidth = 0;
+        int destHeight = 0;
+        String dest = "";
+        //ImageUtils.scale(src, destWidth, destHeight, dest);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
